edb471d5f5f9f42a4654a0f2c730a4f9e9131fd5
Supriyo <72860690+Supsource@users.noreply.github.com>
2023-04-03 23:24:10 +0530

    imported CategorySlidebar.css, some dummy data

--- src/Components/Product/CategorySlidebar.js
+++ src/Components/Product/CategorySlidebar.js
@@ -3,6 +3,7 @@ import img1 from '../../Assets/images/1.png'
 import img2 from '../../Assets/images/2.png'
 import img3 from '../../Assets/images/3.png'
 import img4 from '../../Assets/images/4.png'
+import './CategorySlidebar.css'
 
 const CategorySlidebar = () => {
 
@@ -26,7 +27,17 @@ const CategorySlidebar = () => {
       id:4,
       categoryimage: img4,
       categoryname : 'Category 4'
-    }
+    },
+    {
+      id:5,
+      categoryimage: img2,
+      categoryname : 'Category 5'
+    },
+    {
+      id:6,
+      categoryimage: img1,
+      categoryname : 'Category6'
+    },
   ]
 
   return (
@@ -36,7 +47,7 @@ const CategorySlidebar = () => {
           return(
             <div className='category'>
               <img src={item.categoryimage} alt='categoryimage'/>
-              <p>{item.categoryname}</p>
+              <h3>{item.categoryname}</h3>
             </div>
           )
         })
