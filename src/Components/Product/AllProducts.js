5b2fb2455aaa4238351bbf5f93faddc4bc9c0fe4
Supriyo <72860690+Supsource@users.noreply.github.com>
2023-04-04 23:46:12 +0530

    add some dommy products items

--- src/Components/Product/AllProducts.js
+++ src/Components/Product/AllProducts.js
@@ -1,6 +1,47 @@
 import React from 'react'
+import img1 from '../../Assets/images/1.png'
+import img2 from '../../Assets/images/2.png'
+import img3 from '../../Assets/images/3.png'
+import img4 from '../../Assets/images/4.png'
+import ProductCart from './ProductCart'
+import './AllProducts.css';
+
 
 const AllProducts = () => {
+  const products =[
+    {
+      id:1,
+      productimgage : img1,
+      productname: 'product 1',
+      productprice: 100,
+      counttype : '1 each',
+      discountpercent: 10
+    },
+    {
+      id:2,
+      productimgage : img2,
+      productname: 'product 2',
+      productprice: 100,
+      counttype : '1 each',
+      discountpercent: 10
+    },
+    {
+      id:3,
+      productimgage : img3,
+      productname: 'product 3',
+      productprice: 100,
+      counttype : '1 each',
+      discountpercent: 10
+    },
+    {
+      id:4,
+      productimgage : img4,
+      productname: 'product 4',
+      productprice: 100,
+      counttype : '1 each',
+      discountpercent: 10
+    }
+  ]
   return (
     <div>AllProducts</div>
   )
