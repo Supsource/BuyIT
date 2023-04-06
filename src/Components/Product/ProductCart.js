44d5b8913251697d86b504479ec46bd37738a73c
Supriyo <72860690+Supsource@users.noreply.github.com>
2023-04-07 00:45:56 +0530

    updated product cart to render trending products in home page

--- src/Components/Product/ProductCart.js
+++ src/Components/Product/ProductCart.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import './ProductCart.css'
 
 const ProductCart = ({data}) => {
   return (
@@ -13,10 +14,24 @@ const ProductCart = ({data}) => {
             }
             <span>₹{data.productprice}</span></h3>
         </div>
-        <div className='s3'></div>
-        <div className='addbtn'> </div>
+        <div className='s3'>
+          <p>{data.counttype}</p>
+        </div>
+        <div className='addbtn'>
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+  <path strokeLinecap="round" strokeLinejoin="round" d="M12 9v3.75m9-.75a9 9 0 11-18 0 9 9 0 0118 0zm-9 3.75h.008v.008H12v-.008z" />
+</svg>
+
+<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+  <path strokeLinecap="round" strokeLinejoin="round" d="M12 9v6m3-3H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z" />
+</svg>
+
+
+
+
+        </div>
     </div>
   )
 }
 
-export default ProductCart
+export default ProductCart;
